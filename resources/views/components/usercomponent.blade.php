40b5c7483c5a60b8fa4032361e811522b2029ca4
Barbara Bianco <barbarabiancov@gmail.com>
2023-05-15 16:11:41 -0300

    ajuste

--- resources/views/components/usercomponent.blade.php
+++ resources/views/components/usercomponent.blade.php
@@ -1,8 +1,6 @@
 <div class="form-group col-6">
     <label for="" class="form-label m-0">Usuário:</label>
     <select name="user" id="user" class="form-control">
-        @foreach ($user as $value )
-         <option value="{{$value->id }}" data-email="{{ $value->email }}">  {{ $value->name }}</option>
-        @endforeach
+
     </select>
 </div>
