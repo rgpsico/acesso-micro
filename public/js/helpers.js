36843934577e23d38b3bbf6e44a9a79491697aad
Barbara Bianco <barbarabiancov@gmail.com>
2023-05-16 18:06:03 -0300

    ajuste

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -3,7 +3,7 @@ function execultarApp()
       $.ajax({
             type: "GET",
             dataType: "json",
-            url: 'http://127.0.0.1:8000/executar-comando',
+            url: 'http://127.0.0.1:8000/meu_endpoint',
             success: function (data) {
                 console.log(data);
             }
